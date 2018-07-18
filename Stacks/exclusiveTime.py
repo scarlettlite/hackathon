c82a7d5ed3488f0e6faa6b171fc02df23cc05d8f
scarlettlite <shivanirathore496@gmail.com>
2018-07-18 09:10:34 +0530

    exclusiveTime-first version

--- Stacks/exclusiveTime.py
+++ Stacks/exclusiveTime.py
@@ -8,31 +8,35 @@ class Solution:
         if not n or not logs: return []
         ans = [ 0 for _ in range(n)]
         stack = []
+        prev = None
         for log in logs:
-            if stack and stack[-1].startswith(log[0]):
-                obj = stack.pop()
-                p,s,t = obj.split(':')
-                p, t = int(p), int(t)
-                p1,s1,t1 = log.split(':')
-                p1, t1 = int(p1), int(t1)
-                et = (t1 - t) + 1
-                ans[p] += et
-                if stack:
-                    obj = stack[-1]
-                    ans[int(obj[0])] -= et               
+            if stack :
+                t1 = int(stack[-1].split(':')[-1])
+                t2 = int(log.split(':')[-1])
+                if "end" in log and "start" in stack[-1]:
+                    p = int(log.split(':')[0])
+                    if prev :
+                       t3 = int(prev.split(':')[-1])
+                       ans[p] += t2 - t3
+                    else:
+                        ans[p] += t2 - t1 + 1
+                    prev = log
+                    stack.pop()
+                elif "start" in log:
+                    p = int(stack[-1].split(':')[0])
+                    if prev :
+                       t3 = int(prev.split(':')[-1])
+                       ans[p] += t2 - t3 - 1
+                       prev = None
+                    else:
+                       ans[p] +=  t2 -  t1 
+                    stack.append(log)
             else:
                 stack.append(log)
         return ans
 
-print(Solution().exclusiveTime(4,
-["0:start:0",
- "1:start:2",
- "1:end:5",
- "2:start:7",
- "3:start:8",
- "3:stop:10",
-"2:stop:14",
- "0:end:16"]
+print(Solution().exclusiveTime(1,
+["0:start:0","0:start:2","0:end:5","0:start:6","0:end:6","0:end:7"]
 ))
 
         
