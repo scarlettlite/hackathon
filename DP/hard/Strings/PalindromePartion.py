fc89379995f044aeeeb06a0b0bb1abff363dfddc
scarlettlite <shivanirathore496@gmail.com>
2018-08-18 07:54:55 +0530

    Fix bugs

--- DP/hard/Strings/PalindromePartion.py
+++ DP/hard/Strings/PalindromePartion.py
@@ -14,7 +14,6 @@ class Solution:
         for j in range(n): 
             for l in range(1, j + 2): # substring length
                 i = j + 1 - l
-                x = s[i:j+1]
                 if s[i] == s[j] and (j-i <= 1 or palin[i+1][j-1] == True):
                     palin[i][j] = True
                     if i == 0:
