d07f2dbf18704d708c91dd74be5f0d1f6141f70e
Shivani Rathore <shivanirathore482@gmail.com>
2018-10-07 15:18:21 +0530

    BinarySearch

--- BinarySearch/SearchInASortedRotatedArray.py
+++ BinarySearch/SearchInASortedRotatedArray.py
@@ -1,32 +1,29 @@
 class Solution(object):
     def search(self, nums, target):
-        if len(nums) == 0:
-            return False
-        
-        lo = 0
-        hi = len(nums) - 1 
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if nums[mid] <= nums[hi]:
-                hi = mid
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        if not nums:
+            return -1
+        low = 0
+        high = len(nums) - 1
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > nums[high]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
             else:
-                lo = mid + 1
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid -1
+             
+        return -1 if nums[low] != target else low
 
-        if target > nums[-1]:
-            lo = 0
-            hi -= 1
-        else:
-            hi = len(nums) - 1
-            
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if target < nums[mid]:
-                hi = mid - 1
-            elif target > nums[mid]:
-                lo = mid + 1
-            else:
-                return True
-            
-        return False
-
-print(Solution().search([1,1,3,1],3))
+print(Solution().search([1,3], 3))
