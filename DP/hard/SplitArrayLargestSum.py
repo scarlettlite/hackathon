179ba9038bbed4d48cb2f044fd8430cf2be2bab3
scarlettlite <shivanirathore496@gmail.com>
2019-03-04 14:40:59 +0530

    SplitArrayLargestSum

--- DP/hard/SplitArrayLargestSum.py
+++ DP/hard/SplitArrayLargestSum.py
@@ -2,18 +2,20 @@ class Solution(object):
     def splitArraySameAverage(self, A):
         if len(A)==1: return False
         global_avg = sum(A)/float(len(A))
-        for lenB in range(1, len(A)/2+1):
+        for lenB in range(1, len(A)//2+1):
             if int(lenB*global_avg) == lenB*global_avg:
-                if self.exist(lenB*global_avg, lenB, A):
+                if self.exist(lenB*global_avg, lenB, A, 0):
                     return True
         return False
             
-    def exist(self, tosum, item_count, arr):
+    def exist(self, tosum, item_count, arr, idx):
         if item_count==0:
             return False if tosum else True
-        if item_count > len(arr) or not arr: 
+        if item_count > len(arr) - idx or idx == len(arr): 
             return False
-        if any([self.exist(tosum-arr[0], item_count-1, arr[1:]),
-               self.exist(tosum, item_count, arr[1:])]):
+        if any([self.exist(tosum-arr[idx], item_count-1, arr, idx+1),
+               self.exist(tosum, item_count, arr, idx+1)]):
             return True
         return False
+
+print(Solution().splitArraySameAverage([5,8,6,7]))
